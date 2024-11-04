85a05fbd575ffca3035f8ebe99dd5d5c2fc999f1
diegohenao819 <diegohenao819@gmail.com>
2024-11-04 12:39:22 -0500

    Falta solucionar renderizado condicional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-[90%] mx-auto flex justify-center items-center h-96 bg-green-400">
-      <Button>Take quiz</Button>
+      <Button asChild>
+        <Link href="/quiz">
+        Take Quiz
+        </Link>
+      </Button>
     </div>
   );
 }
